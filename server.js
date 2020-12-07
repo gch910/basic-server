bdd18995e22b1caa966cdb4313cdf841c4293659
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-12-07 15:36:11 -0600

    added more codes

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const server = http.createServer(function (req, res) {
 	if (req.url === '/OK') {
 		console.log("Inbound 'OK' request being processed...");
 		res.writeHead(200);
-	} else {
+    }
+    else if (req.url === '/BAD') {
+        console.log("BAD REQUEST");
+		res.writeHead(400);
+    }
+    else if (req.url === '/CREATED') {
+        console.log("CREATED");
+		res.writeHead(201);
+    }
+    else {
         console.log('Page not found!');
         res.writeHead(404);
     }
