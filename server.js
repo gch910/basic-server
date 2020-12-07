83716a57400398299efe6d3b0beac09ed2a813bc
Nkongchou Fomenky <nfomenky@student.ysu.edu>
2020-12-07 15:27:19 -0600

    added functionality to server

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require('http');
 
-const server = http.createServer(function(req, res) {
-    res.end();
-})
+const server = http.createServer(function (req, res) {
+	if (req.url === '/OK') {
+		console.log("Inbound 'OK' request being processed...");
+		res.writeHead(200);
+	} else {
+        console.log('Page not found!');
+        res.writeHead(404);
+    }
+	res.end();
+});
 
 const port = 3000;
 
-server.listen(port, function() {
-    console.log(`The server is listening on port ${port}`);
-} )
+server.listen(port, function () {
+	console.log(`The server is listening on port ${port}`);
+});
