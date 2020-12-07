6a775aa5cdf425f6f4fc8ed00cd00687e057be5b
Geb <gch910@gmail.com>
2020-12-07 16:04:49 -0600

    final commit

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
 	if (req.url === '/OK') {
 		console.log("Inbound 'OK' request being processed...");
-		res.writeHead(200);
+		res.writeHead(301, {
+            Location: 'localhost:3000/CREATED'
+        });
     }
     else if (req.url === '/BAD') {
         console.log("BAD REQUEST");
 		res.writeHead(400);
     }
     else if (req.url === '/CREATED') {
+        if(req.method === 'POST') {
         console.log("CREATED");
-		res.writeHead(201);
+        res.writeHead(201);
+        res.write('<h1>Hello</h1>')
+        } else {
+            res.writeHead(405)
+        }
+
     }
     else {
         console.log('Page not found!');
